a9f1a9ce36f350a8408fea475cc66612e9fde4dd
Filip Strózik <filip.strozik@outlook.com>
2023-02-14 22:11:22 +0100

    Apply suggestions from code review

--- script.user.js
+++ script.user.js
@@ -1,10 +1,10 @@
 // ==UserScript==
 // @name         Flyff Universe Multi-Tab Spell Caster
-// @namespace    pPrecel, doodlefudge
-// @version      1.0
+// @namespace    pPrecel
+// @version      2.1
 // @description  Helpful tool for the web browser version of the Flyff Universe to cast spells from another tab.
-// @author       pPrecel, dooduelfudge
-// @website      https://github.com/doodlefudge/enhanced-flyff-universe-multi-tab-spell-caster
+// @author       pPrecel, doodlefudge
+// @website      https://github.com/pPrecel/flyff-universe-multi-tab-spell-caster
 // @include      https://universe.flyff.com/play
 // @grant        GM_log
 // @license      MIT
